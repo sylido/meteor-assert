0420da6fdc8ac3dae487e264f7e315078440361d
Danail Gabenski <dgabenski@fortressinfosec.com>
2025-06-25 18:25:57 -0600

    Updated for Meteor 3.x
    Just updated to use the fork sylido at version 0.4.0
    Then use Meteor 3.2.2 or later

--- package.js
+++ package.js
@@ -1,35 +1,35 @@
 Package.describe({
-  summary: "Make code assertions to assure your code works as expected",
-  version: '0.3.0',
-  name: 'peerlibrary:assert',
-  git: 'https://github.com/peerlibrary/meteor-assert.git'
+  summary : "Make code assertions to assure your code works as expected",
+  version : "0.4.0",
+  name    : "sylido:assert",
+  git     : "https://github.com/sylido/meteor-assert.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
-  api.export('assert');
-
-  api.addFiles([
-    'server.js'
-  ], 'server');
+  api.export("assert");
 
   api.addFiles([
-    'client/assert.js',
-    'export-assert.js'
-  ], 'client');
+    "server.js"
+  ], "server");
+
+  api.addFiles([
+    "client/assert.js",
+    "export-assert.js"
+  ], "client");
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
   api.use([
-    'peerlibrary:assert',
-    'tinytest',
-    'test-helpers'
-  ], ['client', 'server']);
+    "sylido:assert",
+    "tinytest",
+    "test-helpers"
+  ], ["client", "server"]);
 
   api.addFiles([
-    'tests.js'
-  ], ['client', 'server']);
+    "tests.js"
+  ], ["client", "server"]);
 });
